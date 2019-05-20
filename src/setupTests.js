f13f48d2b43b08a81ed5bf10f5fab0ca696f6a38
Mehmet Egemen <mehmetegemenalbayrak@gmail.com>
2019-05-20 13:36:31 +0300

    test config added

---
+++ src/setupTests.js
@@ -0,0 +1,6 @@
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+
+expect.extend({ toMatchImageSnapshot });
+configure({ adapter: new Adapter() });
