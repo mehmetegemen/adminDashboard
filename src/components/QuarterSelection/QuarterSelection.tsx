4722560f6829ff45fbfea8baa5110265f90b4a99
Mehmet Egemen <mehmetegemenalbayrak@gmail.com>
2019-05-29 09:03:52 +0300

    QuarterSelection created

---
+++ src/components/QuarterSelection/QuarterSelection.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from "react";
+import QuarterSelectionButton from "./QuarterSelectionButton/QuarterSelectionButton";
+import IQuarter from "../LineChart/IQuarter";
+
+import "./QuarterSelection.scss";
+
+const QuarterSelection: FunctionComponent<{
+  changeQuarterCallback: (
+    quarter: number,
+    event: React.MouseEvent<HTMLDivElement>,
+  ) => void;
+  data: IQuarter[][];
+  currency: string;
+}> = ({ changeQuarterCallback, currency, data }) => {
+  return(
+    <div className="quarter-selection">
+      {data.map((q, i) => (
+        <QuarterSelectionButton
+          changeQuarterCallback={changeQuarterCallback.bind(null, i)}
+          data={data[i]}
+          currency="$"
+          quarter={i + 1}
+        />
+      ))}
+    </div>
+  )
+};
+
+export default QuarterSelection;
